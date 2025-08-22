0bf0c879d385b789ac8edba0517bac3491adde0f
JoelleTindall <jtindall222@gmail.com>
2025-08-22 16:52:58 -0400

    initial

---
+++ app/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="flex flex-col min-h-dvh max-w-dvw">
+
+    <NuxtPage />
+</div>
+
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div>
+<h1>walcome to tindall art .com</h1>
+   </div>
+  </main>
+</template>
+
+<script setup>
+  useSeoMeta({
+    title: 'Tindallart - Artworks of Karen and Dana Tindall',
+    ogTitle: 'Tindallart', // Open Graph title
+    description: 'Cincinnati based artists Karen Tindall and Dana Tindall',
+  });
+</script>

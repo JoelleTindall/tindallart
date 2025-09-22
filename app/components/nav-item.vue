b4338496ed4f79126393cd30d5012aaae7b829d8
JoelleTindall <jtindall222@gmail.com>
2025-09-22 17:26:54 -0400

    test deploy

--- app/components/nav-item.vue
+++ app/components/nav-item.vue
@@ -5,8 +5,8 @@
         :class="[isActive ? ('text-shadow-gray-100/40 text-shadow-md') : ('text-shadow-2xs text-shadow-transparent')]">
         <div 
             class="  group-hover:bg-blueb transition h-24 content-center"
-            :class="[isActive ? ('bg-choco') : ('bg-none'), isActive && 'mobileLandscape:bg-lightgreen']">
-            <div class="w-full px-4 group-hover:py-6 group-active:py-6 transition-padding duration-400 group-hover:bg-blueb group-active:bg-choco flex content-center justify-center">
+            :class="[isActive ? ('bg-blueb') : ('bg-none')]">
+            <div class="w-full px-4 group-hover:py-6 group-active:py-6 transition-padding duration-400 group-hover:bg-blueb group-active:bg-blueb flex content-center justify-center">
                 <p 
                     class="link font-normal mobileLandscape:text-lg  group-hover:text-offwhite group-hover:scale-105 group-hover:cursor-pointer group-active:scale-105 transition-all"
                     :class="[isActive ? ('text-offwhite scale-105') : ('text-offblack/95'), isActive && 'mobileLandscape:text-offwhite scale-105']">{{ name }}
